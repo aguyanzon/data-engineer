c13c3cc3b9d578257f89463e7b9d992fc8828d7e
Agustin Yanzon <aguyanzon@hotmail.com>
2023-02-20 16:23:55 -0300

    Update big_query_hw.sql

--- week_3_data_warehouse/homework/big_query_hw.sql
+++ week_3_data_warehouse/homework/big_query_hw.sql
@@ -1,19 +1,42 @@
 -- Creating external table referring to gcs path
-CREATE OR REPLACE EXTERNAL TABLE `dtc-de-375812.fhv_table.ny_taxis`
+CREATE OR REPLACE EXTERNAL TABLE `dtc-de-375812.ny_taxi.fhv_external_data`
 OPTIONS (
-  format = 'CSV',
-  uris = ['gs://dtc_data_lake_dtc-de-375812/fhv/fhv_tripdata_2019-*.csv.gz']
+  format = 'PARQUET',
+  uris = ['gs://dtc_data_lake_dtc-de-375812/fhv/fhv_tripdata_2019-*.parquet']
 );
 
 -- Check yellow trip data
-SELECT COUNT(*) FROM `fhv_table.ny_taxis`;
+SELECT COUNT(*) FROM `ny_taxi.fhv_external_data`;
+
+SELECT COUNT(DISTINCT(affiliated_base_number)) FROM `ny_taxi.fhv_external_data`;
+SELECT COUNT(DISTINCT(affiliated_base_number)) FROM `ny_taxi.fhv_tripdata`;
+
+SELECT COUNT(*) FROM `ny_taxi.fhv_external_data` AS t
+WHERE t.DOlocationID IS NULL AND t.PUlocationID IS NULL;
+
+CREATE OR REPLACE TABLE ny_taxi.fhv_tripdata_cast (
+  `dispatching_base_num` STRING,
+  `pickup_datetime` DATETIME,
+  `dropOff_datetime` DATETIME,
+  `PUlocationID` FLOAT64,
+  `DOlocationID` FLOAT64,
+  `SR_Flag` FLOAT64,
+  `Affiliated_base_number` STRING 
+) AS
+SELECT dispatching_base_num, CAST(pickup_datetime AS DATETIME), CAST(dropOff_datetime AS DATETIME), PUlocationID, DOlocationID, SR_Flag, Affiliated_base_number FROM ny_taxi.fhv_external_data;
 
 -- Create a non partitioned table from external table
-CREATE OR REPLACE TABLE fhv_table.ny_taxis_non_partitoned AS
-SELECT * FROM fhv_table.ny_taxis;
+CREATE OR REPLACE TABLE ny_taxi.fhv_non_partitoned_tripdata AS
+SELECT * FROM ny_taxi.fhv_tripdata_cast;
 
 -- Create a partitioned table from external table
-CREATE OR REPLACE TABLE fhv_table.ny_taxis_partitoned
+CREATE OR REPLACE TABLE ny_taxi.fhv_partitoned_tripdata
 PARTITION BY
   DATE(pickup_datetime) AS
-SELECT * FROM fhv_table.ny_taxis;
+SELECT * FROM ny_taxi.fhv_tripdata_cast;
+
+SELECT DISTINCT(Affiliated_base_number) FROM `ny_taxi.fhv_non_partitoned_tripdata`
+WHERE DATE(pickup_datetime) BETWEEN '2019-03-01' AND '2019-03-31';
+
+SELECT DISTINCT(Affiliated_base_number) FROM `ny_taxi.fhv_partitoned_tripdata`
+WHERE DATE(pickup_datetime) BETWEEN '2019-03-01' AND '2019-03-31';
